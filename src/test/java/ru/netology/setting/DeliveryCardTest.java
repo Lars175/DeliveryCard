e2f8a9af120a24315749ed281fc39746fa9c3bf6
Larisa Karelina <karelina.rss@gmail.com>
2020-07-23 14:45:58 +0300

    bug fix

--- src/test/java/ru/netology/setting/DeliveryCardTest.java
+++ src/test/java/ru/netology/setting/DeliveryCardTest.java
@@ -6,13 +6,21 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryCardTest {
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    //    присваиваем формат даты
+    LocalDate todayPlusThree = LocalDate.now().plusDays(3);
+    //    прибавляет дни к текущей дате
+    String dateToEnter = todayPlusThree.format(formatter);
 
     @Test
     void orderDeliveryRequest() {
@@ -20,15 +28,21 @@ public class DeliveryCardTest {
         SelenideElement form = $(".form");
 
         form.$("[data-test-id=city] input").setValue("Ярославль");
-        form.$("[data-test-id=date] input").setValue("23.07.2020");
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(dateToEnter);
         form.$("[data-test-id=name] input").setValue("Иванов Анатолий");
-        form.$("[data-test-id=phone] input").setValue("+79991992949");
+        form.$("[data-test-id=phone] input").setValue("+31961992949");
+
 
         form.$("[data-test-id=agreement]").click();
         form.$$("[role=button]").find(exactText("Запланировать")).click();
 
         $(withText("Встреча успешно запланирована на")).waitUntil(visible, 15000);
+        $(withText("Встреча успешно запланирована на")).shouldHave(text(dateToEnter));
     }
+
+
+
 
     @Test
     void orderDeliveryRequestRepeat() {
@@ -36,9 +50,11 @@ public class DeliveryCardTest {
         SelenideElement form = $(".form");
 
         form.$("[data-test-id=city] input").setValue("Ярославль");
-        form.$("[data-test-id=date] input").setValue("23.07.2020");
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(dateToEnter);
         form.$("[data-test-id=name] input").setValue("Иванов Анатолий");
-        form.$("[data-test-id=phone] input").setValue("+79991992949");
+        form.$("[data-test-id=phone] input").setValue("+31961992949");
+
 
         form.$("[data-test-id=agreement]").click();
         form.$$("[role=button]").find(exactText("Запланировать")).click();
@@ -52,7 +68,8 @@ public class DeliveryCardTest {
         SelenideElement form = $(".form");
 
         form.$("[data-test-id=city] input").setValue("Порт");
-        form.$("[data-test-id=date] input").setValue("23.07.2020");
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(dateToEnter);
         form.$("[data-test-id=name] input").setValue("Иванов Анатолий");
         form.$("[data-test-id=phone] input").setValue("+79991992949");
 
@@ -68,7 +85,8 @@ public class DeliveryCardTest {
         SelenideElement form = $(".form");
 
         form.$("[data-test-id=city] input").setValue("Москва");
-        form.$("[data-test-id=date] input").setValue("23.07.2020");
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(dateToEnter);
         form.$("[data-test-id=name] input").setValue("");
         form.$("[data-test-id=phone] input").setValue("+79991992949");
 
@@ -84,7 +102,8 @@ public class DeliveryCardTest {
         SelenideElement form = $(".form");
 
         form.$("[data-test-id=city] input").setValue("Москва");
-        form.$("[data-test-id=date] input").setValue("23.07.2020");
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(dateToEnter);
         form.$("[data-test-id=name] input").setValue("Igor Petrov");
         form.$("[data-test-id=phone] input").setValue("+79991992949");
 
@@ -101,9 +120,9 @@ public class DeliveryCardTest {
 
         form.$("[data-test-id=city] input").setValue("Москва");
         form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
-        form.$("[data-test-id=date] input").setValue("23.07.2020");
+        form.$("[data-test-id=date] input").setValue("17.08.2020");
         form.$("[data-test-id=name] input").setValue("Иван Иванов");
-        form.$("[data-test-id=phone] input").setValue("12345678");
+        form.$("[data-test-id=phone] input").setValue("123456");
 
         form.$("[data-test-id=agreement]").click();
         form.$$("[role=button]").find(exactText("Запланировать")).click();
@@ -117,7 +136,8 @@ public class DeliveryCardTest {
         SelenideElement form = $(".form");
 
         form.$("[data-test-id=city] input").setValue("Москва");
-        form.$("[data-test-id=date] input").setValue("23.07.2020");
+        form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a", Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(dateToEnter);
         form.$("[data-test-id=name] input").setValue("Иван Иванов");
         form.$("[data-test-id=phone] input").setValue("+0 000 000 000");
 
